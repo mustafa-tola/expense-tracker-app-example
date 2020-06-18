7c1b6c2955357a1e880c4d4a84205d3c07afcbc7
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2020-06-18 17:56:57 +0500

    changed and completed the UI

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,13 +2,18 @@ import React, { useContext } from 'react';
 import { GlobalContext } from "../context/GlobalState"
 
 export const Transaction = ({ transaction }) => {
-    const { deleteTransaction,editTransaction } = useContext(GlobalContext)
+    const { deleteTransaction, editTransaction } = useContext(GlobalContext)
     const sign = transaction.changeableAmount < 0 ? "-" : "+"
     return (
+        <>
         <ul className="list">
             <li className={transaction.changeableAmount < 0 ? "minus" : "plus"} >
-                {transaction.textDesc} <span>{sign}${Math.abs(transaction.changeableAmount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">X</button><button style={{ backgroundColor: "black",color: "white" }} onClick={() => editTransaction(transaction.id)}>Edit</button>
+                {transaction.textDesc} <span>{sign}${Math.abs(transaction.changeableAmount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">X</button><button style={{ backgroundColor: "black", color: "white" }} onClick={() => editTransaction(transaction.id)}>Edit</button>
             </li>
         </ul>
+        <input type="text" value={transaction.textDesc} width="5" />
+        <input type="number" value={transaction.changeableAmount} />
+
+        </>
     )
 }
