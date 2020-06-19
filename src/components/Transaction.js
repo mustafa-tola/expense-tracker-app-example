d18ab757fbb69eb2460611e1acb0f096e601e543
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2020-06-19 12:02:58 +0500

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { GlobalContext } from "../context/GlobalState"
 
 export const Transaction = ({ transaction }) => {
-    const { deleteTransaction, editTransaction, edited, id, afterEdit } = useContext(GlobalContext)
+    const { deleteTransaction, id,  } = useContext(GlobalContext)
     const sign = transaction.changeableAmount < 0 ? "-" : "+";
     const editStyle = {};
     const viewStyle = {};
@@ -13,15 +13,6 @@ export const Transaction = ({ transaction }) => {
         editStyle.display = "none";
     }
 
-    const doneEdit = (e) => {
-        console.log("Editing is Done");
-        if (e.keyCode === 13) {
-            console.log(e.keyCode)
-            afterEdit();
-        }
-    }
-
-
     return (
         <>
             <ul className="list">
@@ -29,8 +20,8 @@ export const Transaction = ({ transaction }) => {
                     {transaction.textDesc} <span>{sign}${Math.abs(transaction.changeableAmount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">X</button>
                 </li>
             </ul>
-            <input type="text" style={transaction.id == id ? none : editStyle} value={transaction.textDesc} />
-            <input type="number" style={transaction.id == id ? none : editStyle} value={transaction.changeableAmount}/>
+            <input type="text" value={transaction.textDesc} />
+            <input type="number" value={transaction.changeableAmount}/>
         </>
     )
 }
